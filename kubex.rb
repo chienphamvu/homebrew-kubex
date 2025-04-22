afcbf98f89666c62c0857235644b5aaa8eaf00c3
Chien Pham <chien.pham@unifiedpost.com>
2025-04-22 22:42:41 +0700

    Release v0.2.1

--- kubex.rb
+++ kubex.rb
@@ -1,9 +1,9 @@
 class Kubex < Formula
   desc "kubectl eXtreme - make your kubectl experience a breeze"
   homepage "https://github.com/chienphamvu/kubex"
-  url "https://github.com/chienphamvu/kubex/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "93c55313d2d06627c87f4237e95f3d88ad9cfbbd5ce9c001af548d675d127fb4"
-  version "v0.2.0"
+  url "https://github.com/chienphamvu/kubex/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "e7fc5aca804c688abe25359d5d93520ba1948a1eb996bc71e121408d89ffe74e"
+  version "v0.2.1"
 
   def install
     bin.install "kubex"
